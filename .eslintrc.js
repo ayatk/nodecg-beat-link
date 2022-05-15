f4263d45bb6a656ade3e7572f5dea9f59d1db4ba
ayatk <git@ayatk.com>
2022-05-16 01:07:59 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+"use strict"
+
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["import"],
+  env: {
+    es2022: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    "sort-imports": 0,
+    "import/order": [2, { alphabetize: { order: "asc" } }],
+  },
+  parserOptions: { project: "./tsconfig.json" },
+  ignorePatterns: [".eslintrc.*", "vite.config.*"],
+}
